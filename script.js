b286c2c68205b8cb628349d4358d9528973d08cd
germansoria05 <germanisoria05@gmail.com>
2023-09-26 12:24:27 -0300

    final JS

--- script.js
+++ script.js
@@ -1,67 +1,70 @@
-// Objeto para almacenar la lista de tareas
-const listaDeTareas = {
-        tareas: [],
-        agregarTarea: function (tareaTexto) {
-          this.tareas.push({
-            texto: tareaTexto,
-            completada: false
-          });
-        }
-        marcarTareaComoCompletada: function (indice) {
-          if (indice >= 0 && indice < this.tareas.length) {
-            this.tareas[indice].completada = true;
-          }
-        }
-        eliminarTarea: function (indice) {
-          if (indice >= 0 && indice < this.tareas.length) {
-            this.tareas.splice(indice, 1);
-          }
-        }
-        obtenerTareasCompletadas: function () {
-          return this.tareas.filter(tarea => tarea.completada);
-        }
-      };
-      
-      // Función para mostrar la lista de tareas
-      function mostrarListaDeTareas() {
-        console.log('Lista de tareas:');
-        listaDeTareas.tareas.forEach(function (tarea, indice) {
-          const estado = tarea.completada ? 'Completada' : 'Pendiente';
-          console.log(`${indice + 1}. [${estado}] ${tarea.texto}`);
-        });
-      }
-      
-      // Función principal para ejecutar la aplicación
-      function ejecutarAppDeTareas() {
-        while (true) {
-          const opcion = prompt(
-            'Opciones:\n1. Agregar tarea\n2. Marcar tarea como completada\n3. Eliminar tarea\n4. Salir'
-          );
-      
-          switch (opcion) {
-            case '1':
-              const nuevaTarea = prompt('Ingrese la nueva tarea:');
-              listaDeTareas.agregarTarea(nuevaTarea);
-              break;
-            case '2':
-              mostrarListaDeTareas();
-              const indiceCompletada = parseInt(prompt('Ingrese el número de la tarea completada:')) - 1;
-              listaDeTareas.marcarTareaComoCompletada(indiceCompletada);
-              break;
-            case '3':
-              mostrarListaDeTareas();
-              const indiceEliminar = parseInt(prompt('Ingrese el número de la tarea a eliminar:')) - 1;
-              listaDeTareas.eliminarTarea(indiceEliminar);
-              break;
-            case '4':
-              return; 
-              // Salir de la aplicación
-            default:
-              console.log('Opción no válida. Por favor, elija una opción válida.');
-          }
-          mostrarListaDeTareas();
-        }
-      }
-      
-      // Ejecutar la aplicación
-      ejecutarAppDeTareas();
+
+function calculateLoan(userName, userEmail, loanAmount, annualInterestRate, loanTerm) {
+    if (isNaN(loanAmount) || isNaN(annualInterestRate) || isNaN(loanTerm)) {
+        alert('Por favor, ingrese valores numéricos válidos.');
+        return null;
+    }
+    
+    const monthlyInterestRate = (annualInterestRate / 100) / 12;
+
+    let remainingBalance = loanAmount;
+    let totalInterest = 82;
+
+    for (let month = 1; month <= loanTerm; month++) {
+        const interestPayment = remainingBalance * monthlyInterestRate;
+        totalInterest += interestPayment;
+        const principalPayment = (loanAmount / loanTerm) - interestPayment;
+        remainingBalance -= principalPayment;
+    }
+
+    const monthlyPayment = loanAmount / loanTerm;
+
+    return {
+        userName: userName,
+        userEmail: userEmail,
+        monthlyPayment: monthlyPayment.toFixed(2),
+        totalPayment: (loanAmount + totalInterest).toFixed(2),
+        totalInterest: totalInterest.toFixed(2)
+    };
+}
+
+function displayResult(result) {
+    const resultElement = document.querySelector('#result');
+    resultElement.innerHTML = `
+        <h2>Resultado:</h2>
+        <p>Nombre: ${result.userName}</p>
+        <p>Correo Electrónico: ${result.userEmail}</p>
+        <p>Pago Mensual: $${result.monthlyPayment}</p>
+        <p>Pago Total: $${result.totalPayment}</p>
+        <p>Interés Total: $${result.totalInterest}</p>
+    `;
+}
+
+function handleCalculateButtonClick() {
+    const userName = document.querySelector('#userName').value;
+    const userEmail = document.querySelector('#userEmail').value;
+    const loanAmount = parseFloat(document.querySelector('#loanAmount').value);
+    const annualInterestRate = parseFloat(document.querySelector('#annualInterestRate').value);
+    const loanTerm = parseInt(document.querySelector('#loanTerm').value);
+
+    const result = calculateLoan(userName, userEmail, loanAmount, annualInterestRate, loanTerm);
+
+    if (result !== null) {
+        displayResult(result);
+    }
+
+    
+}
+
+function handleResetButtonClick() {
+    document.querySelector('#userName').value = '';
+    document.querySelector('#userEmail').value = '';
+    document.querySelector('#loanAmount').value = '';
+    document.querySelector('#annualInterestRate').value = '';
+    document.querySelector('#loanTerm').value = '';
+    document.querySelector('#result').innerHTML = '';
+}
+
+document.querySelector('#calculateButton').addEventListener('click', handleCalculateButtonClick);
+document.querySelector('#resetButton').addEventListener('click', handleResetButtonClick);
+
